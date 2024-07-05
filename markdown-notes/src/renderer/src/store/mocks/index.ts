fb319ee5088ac1253463b72a331a5fc036ff9149
salgue441 <carlossalguero441@gmail.com>
2024-07-04 22:26:27 -0600

    feat(actions): added missing run command in build step

---
+++ markdown-notes/src/renderer/src/store/mocks/index.ts
@@ -0,0 +1,16 @@
+import { NoteInfo } from '@shared/models'
+
+export const notesMock: NoteInfo[] = [
+  {
+    title: 'Welcome 👋🏻',
+    lastEditTime: new Date().getTime()
+  },
+  {
+    title: 'Getting Started',
+    lastEditTime: new Date().getTime()
+  },
+  {
+    title: 'Customization Options',
+    lastEditTime: new Date().getTime()
+  }
+]
